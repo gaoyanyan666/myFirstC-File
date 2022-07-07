1c4c308db6433d95dd866fe61b67fc70d799db93
annagao <ggaoyanyan@gmail.com>
2022-07-07 10:14:09 -0700

    my first git commit

---
+++ demo.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+
+int main(){
+    string username;
+    cout << "Hello world!";
+    cout << "\n";
+    cout << "what is your name?";
+    cin >> username;
+    cout << "your name is " << username;
+    return 0;
+}
